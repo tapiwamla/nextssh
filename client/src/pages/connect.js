93619d013e8b71f7571f53b68f9aa7e137e03e58
Tapiwanashe Mlambo <tmlambo@usiu.ac.ke>
2024-02-20 22:32:43 +0300

    Add form validation, navigate to terminal on submit

--- client/src/pages/connect.js
+++ client/src/pages/connect.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 import BaseLayout from '../components/BaseLayout';
 
+import { useRouter } from 'next/router'
+
 const Connection = () => {
   const [host, setHost] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const router = useRouter()
+
   const handleSubmit = (event) => {
-    event.preventDefault();
-    // Handle form submission here
-  };
+    event.preventDefault()
+
+    router.push('/terminal')
+  }
 
   return (
     <BaseLayout pageTitle="CONNECT A NEW RESOURCE">
@@ -17,15 +22,15 @@ const Connection = () => {
         <form onSubmit={handleSubmit}>
           <label>
             Host IP:
-            <input type="text" value={host} onChange={(e) => setHost(e.target.value)} />
+            <input type="text" value={host} onChange={(e) => setHost(e.target.value)} pattern="((^|\.)((25[0-5])|(2[0-4]\d)|(1\d\d)|([1-9]?\d))){4}$" title="For example: 127.0.0.1" required />
           </label>
           <label>
             Username:
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
           </label>
           <label>
             Password:
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
           </label>
           <button type="submit">Connect</button>
         </form>
