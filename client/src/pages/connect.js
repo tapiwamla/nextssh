a92f619d849ee93ea45e858aab8a85cea538b431
Tapiwanashe Mlambo <tmlambo@usiu.ac.ke>
2024-02-20 21:06:46 +0300

    Removed non-essential form fields

--- client/src/pages/connect.js
+++ client/src/pages/connect.js
@@ -2,11 +2,9 @@ import React, { useState } from 'react';
 import BaseLayout from '../components/BaseLayout';
 
 const Connection = () => {
-  const [hostName, setHostName] = useState('');
   const [ipAddress, setIpAddress] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [authKey, setAuthKey] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -18,10 +16,6 @@ const Connection = () => {
       <div className="form-wrapper">
         <form onSubmit={handleSubmit}>
           <label>
-            Host Name:
-            <input type="text" value={hostName} onChange={(e) => setHostName(e.target.value)} />
-          </label>
-          <label>
             IP Address:
             <input type="text" value={ipAddress} onChange={(e) => setIpAddress(e.target.value)} />
           </label>
@@ -32,10 +26,6 @@ const Connection = () => {
           <label>
             Password:
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          </label>
-          <label>
-            Private Key:
-            <input className="text-area" type="textarea" value={authKey} onChange={(e) => setAuthKey(e.target.value)} />
           </label>
           <button type="submit">Connect</button>
         </form>
