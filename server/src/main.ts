caa6c5a76dbaee33adeff73c6fa64671f87d7f59
Tapiwanashe Mlambo <tmlambo@usiu.ac.ke>
2024-02-21 14:06:45 +0300

    Added console messages, for debugging

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,17 +4,29 @@ const expressWs = require('express-ws')(app);
 
 import { createNewServer } from './utils/createNewServer';
 
+const server = app.listen(3001, () => {
+  console.log('Server listening on port 3001');
+});
 
 app.get('/', function (req: any, res: any, next: any) {
+  console.log('GET request received on /');
   res.end();
 });
 
 app.ws('/', function (ws: any, req: any) {
+  console.log('WebSocket connection established');
+
+  ws.on('error', (error: any) => {
+    console.error('WebSocket error:', error);
+  });
+
   createNewServer({
     host: '172.210.83.226',
     username: 'ubuntu',
     password: 'fail2Banfail2Ban'
-  }, ws)
+  }, ws);
 });
 
-app.listen(3001)
+server.on('error', (error: any) => {
+  console.error('Server failed to start:', error);
+});
