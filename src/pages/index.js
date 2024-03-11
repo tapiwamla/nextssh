c34b464d9e90fe512075e9d09b7ebfad09f49d30
Tapiwanashe Mlambo <tmlambo@usiu.ac.ke>
2024-03-11 16:39:21 +0300

    Added landing page with Github and Google logins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import { useSession, signIn, signOut } from "next-auth/react";
-import { useRouter } from 'next/router'; // Import useRouter
+import { useRouter } from 'next/router';
+import { FaGithub, FaGoogle } from "react-icons/fa";
 
-export default function SignInPage() {
-  const { data: session } = useSession();
-  const router = useRouter(); // Initialize router
+const Index = () => {
+  const { data: session, status } = useSession();  
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
 
   if (session) {
     return (
@@ -14,14 +19,45 @@ export default function SignInPage() {
     );
   }
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn("github", { callbackUrl: '/connect' })}>
-        Sign in with GitHub
-      </button>
-      <button onClick={() => signIn("google", { callbackUrl: '/connect' })}>
-        Sign in with Google
-      </button>
-    </>
+    <div className="main-container"> 
+      <div className="left-section">
+        <img className="landing-image" src="/servers.jpg" alt="Servers" />
+      </div>
+      <div className="right-section">
+        <h1 className="landing-title">CShell: SSH on the Web</h1>
+        <p>Connect to and command your remote servers effortlessly with CShell, a web-based SSH client and terminal emulator. Master your systems directly from your browser!</p>
+        {status === "loading" && <div>Loading...</div>}
+        {!session && (
+          <>
+            <div className="signin-buttons">
+            <button 
+                className="signin-button" 
+                onClick={() => signIn("google", { 
+                  callbackUrl: `${window.location.origin}/connect`
+                })}
+              >
+                <FaGoogle className="landing-icon" /> Continue with Google
+              </button>
+              <button 
+                className="signin-button" 
+                onClick={() => signIn("github", {
+                  callbackUrl: `${window.location.origin}/connect`,
+                })}
+              >
+                <FaGithub className="landing-icon"/> Continue with GitHub
+              </button>
+            </div>
+          </>
+        )}
+        {session && (
+          <>
+            Signed in as {session.user.email} <br />
+            <button className="signout-button" onClick={() => signOut()}>Sign out</button>
+          </>
+        )}
+      </div>
+    </div>
   );
 }
+
+export default Index;
